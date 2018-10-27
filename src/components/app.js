0eb3f4a856300028b9292fe930d9edffb1758ef4
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-10-27 07:18:49 -0700

    setup redux form in searc.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import Logo from './logo';
 import SearchBar from './searchBar';
 import RecentPosts from './recentPosts';
@@ -7,9 +8,11 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <div>
           <Logo/>
           <SearchBar/>
           <RecentPosts/>
+        </div>
       </div>
     );
   }
